42cb455ad517451d2f5dad7d8bc76cdd55591fdb
prashant sharma <prashant.sharma@enquero.com>
2020-06-23 15:04:27 +0530

    mongo connection and boiler plate complete, need to start adding graphql queries and resolvers

--- db/index.js
+++ db/index.js
@@ -1,10 +1,30 @@
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config.json")[env];
+const MongoClient = require("mongodb").MongoClient;
+const logger = require("../utils/logger");
 
-const db = {};
+// Connection URL
+const dbUrl = config.dbUrl;
+// Database Name
+const dbName = config.database;
 
+let db = {};
+
+// Use connect method to connect to the server
 const connect = async () => {
-  
-}
+  logger.info("Connecting to DB");
 
-module.exports = { instance: db, connect }
+  try {
+    client = await MongoClient.connect(dbUrl);
+
+    logger.info("Connected to DB");
+    db = client.db(dbName);
+    
+    return Promise.resolve(true);
+  } catch (err) {
+    logger.error(err);
+    return Promise.reject(false);
+  }
+};
+
+module.exports = { instance: db, connect };
